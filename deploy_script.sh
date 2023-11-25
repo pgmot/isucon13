86e44f7b3f64c2b10e9283bed3d490da737b106a
mot <mot@pgmot.com>
2023-11-25 02:16:30 +0000

    fix

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,65 +1,40 @@
- #!/bin/bash
- # Usage: deploy.sh [BRANCH]
- # 
- # Arguments
- #   BRANCH  branch name. default is "master"
- 
- set -eux
- set -o pipefail
- 
- APP="isupipe"
- LANG="go"
- SERVICE="$APP-$LANG.service"
- ISU1="ip-192-168-0-11"
- ISU2="ip-192-168-0-13"
- ISU3="ip-192-168-0-12"
- DEFAULT_BRANCH="master"
- 
- #### ARGS
- branch="${1:-"$DEFAULT_BRANCH"}"
- 
- #### PREPARE
- cd "$(dirname "$0")"
- git fetch
- git reset --hard "origin/$branch"
- # git rebase "origin/$1"
- 
- #### STOP
- sudo systemctl stop "${SERVICE}"
- 
- case "$(hostname)" in
- "$ISU1")
-     ;;
- "$ISU2")
-     # sudo systemctl stop nginx
-     ;;
- "$ISU3")
-     ;;
- esac
- 
- #### AFTER STOP
- [ -f "/var/log/nginx/access.log" ] && sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date "+%Y%m%d_%H%M%S"`
- [ -f "/var/log/nginx/error.log" ] && sudo mv /var/log/nginx/error.log /var/log/nginx/error.log.`date "+%Y%m%d_%H%M%S"`
- [ -f "/var/log/mysql/slow_query.log" ] && sudo mv /var/log/mysql/slow_query.log /var/log/mysql/slow_query.log.`date "+%Y%m%d_%H%M%S"`
- 
- # [ "`hostname`" = "$ISU1" ] && sudo systemctl restart mariadb
- 
- #### BUILD
- cd $LANG
- # ./setup.sh
- make build
- 
- #### START
- sudo systemctl start "${SERVICE}"
- 
- case "$(hostname)" in
- "$ISU1")
-     ;;
- "$ISU2")
-     # sudo systemctl s nginx
-     # sudo systemctl restart openresty
-     ;;
- "$ISU3")
-     ;;
- esac
-    
+#!/bin/bash
+set -eux
+set -o pipefail
+
+export PATH="$HOME/local/go/bin:/home/isucon/go/bin:$PATH"
+export GOROOT="$HOME/local/go"
+
+BRANCH="${1:-"master"}"
+
+cd "$(dirname "$0")"
+if [ -d ".git" ]; then
+	git fetch
+	git checkout "$BRANCH"
+	git reset --hard origin/"$BRANCH"
+fi
+
+sudo test -f "/var/log/nginx/access.log" && sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date "+%Y%m%d_%H%M%S"`
+sudo test -f "/var/log/nginx/error.log" && sudo mv /var/log/nginx/error.log /var/log/nginx/error.log.`date "+%Y%m%d_%H%M%S"`
+sudo test -f "/var/log/mysql/mysql-slow.log" && sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.`date "+%Y%m%d_%H%M%S"`
+
+cd go/
+make
+
+hostname="$(hostname)"
+case "${hostname}" in
+	"ip-192-168-0-11") # isucon1
+		sudo systemctl restart nginx
+		sudo systemctl restart isupipe-go.service
+        sudo systemctl restart mysql
+		;;
+	"ip-192-168-0-13") # isucon2
+		#sudo systemctl restart mysql
+		;;
+	"ip-192-168-0-12") # isucon3
+		#sudo systemctl restart isupipe-go.service
+		;;
+	*)
+		echo "${hostname} Didn't match anything"
+		exit 1
+esac
